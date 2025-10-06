b268881356752c1b272dbf36d65f2d7a1e9489a8
nuna2025 <xoddlnuna2017@gmail.com>
2025-10-06 20:50:25 +0200

    feat: Implement sprite jump mechanic

--- Assets/BirdJump.cs
+++ Assets/BirdJump.cs
@@ -2,15 +2,20 @@ using UnityEngine;
 
 public class BirdJump : MonoBehaviour
 {
+    Rigidbody2D rb;
+    public float jumpPower;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            rb.linearVelocity = Vector2.up * jumpPower;
+        }
     }
 }
